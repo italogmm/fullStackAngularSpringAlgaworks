1865e3ca2d225da084732b6350e7cf142481b0ec
Italo Gustavo Miranda Melo <italogmm@gmail.com>
2018-11-08 21:31:52 -0200

    ..
    ..

---
+++ algamoney-api/src/main/java/com/example/algamoney/api/resource/LancamentoResource.java
@@ -0,0 +1,54 @@
+package com.example.algamoney.api.resource;
+
+import com.example.algamoney.api.event.RecursoCriadoEvent;
+import com.example.algamoney.api.model.Categoria;
+import com.example.algamoney.api.model.Lancamento;
+import com.example.algamoney.api.model.Pessoa;
+import com.example.algamoney.api.repository.LancamentoRepository;
+import com.example.algamoney.api.repository.PessoaRepository;
+import com.example.algamoney.api.service.PessoaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/lancamentos")
+public class LancamentoResource {
+
+	@Autowired
+	private LancamentoRepository lancamentoRepository;
+
+	@Autowired
+	private ApplicationEventPublisher publisher;
+
+	@Autowired
+	private PessoaService pessoaService;
+
+	@PostMapping
+	public ResponseEntity<Lancamento> criar(@RequestBody @Valid Lancamento lancamento, HttpServletResponse response){
+
+		Lancamento lancamentoSalvo = lancamentoRepository.save(lancamento);
+		publisher.publishEvent(new RecursoCriadoEvent(this, response, lancamentoSalvo.getCodigo()));
+
+		return ResponseEntity.status(HttpStatus.CREATED).body(lancamentoSalvo);
+	}
+
+	@GetMapping("/{codigo}")
+	public ResponseEntity<Lancamento> buscarPeloCodigo(@PathVariable Long codigo) {
+		Lancamento lancamento = lancamentoRepository.findOne(codigo);
+		return lancamento != null ? ResponseEntity.ok(lancamento) : ResponseEntity.notFound().build();
+	}
+
+	@GetMapping
+	public List<Lancamento> listar(){
+		List<Lancamento> lancamentos = lancamentoRepository.findAll();
+		return lancamentos;
+	}
+
+}
