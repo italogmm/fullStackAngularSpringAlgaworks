06bab283de0dfcc93c376e658ff51656865d2ea6
Italo Gustavo Miranda Melo <italogmm@gmail.com>
2018-11-07 22:59:25 -0200

    Backend fullstack
    Backend fullstack

---
+++ algamoney-api/src/main/java/com/example/algamoney/api/resource/CategoriaResource.java
@@ -0,0 +1,26 @@
+package com.example.algamoney.api.resource;
+
+import com.example.algamoney.api.model.Categoria;
+import com.example.algamoney.api.repository.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/categorias")
+public class CategoriaResource {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    @GetMapping
+    public List<Categoria> listar(){
+        List<Categoria> categorias = categoriaRepository.findAll();
+        return categorias;
+        //return categorias.isEmpty() ? ResponseEntity.noContent().build() : ResponseEntity.ok(categorias);
+    }
+}
